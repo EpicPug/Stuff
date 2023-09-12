23c3b4bcbcccf9a4cae201ab952c305229442a99
EpicPug <112693990+EpicPug@users.noreply.github.com>
2023-09-12 15:42:34 -0500

    Update Webhook.lua

--- Webhook.lua
+++ Webhook.lua
@@ -1,1 +1,10 @@
-
+function Send(a, b, c)
+	local d = {content="",embeds={{title=c[1],description=c[2],type="rich"}}};
+	local e = {Url=a,Body=game.HttpService:JSONEncode(d),Method="POST",Headers={["content-type"]="application/json"}};
+	local f, g = pcall(function()
+		b(e);
+	end);
+	if (not f and g) then
+		warn(g);
+	end
+end
