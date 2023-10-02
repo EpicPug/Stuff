159c53434e0ba81655544be870b855e39658ea4b
EpicPug <112693990+EpicPug@users.noreply.github.com>
2023-10-01 20:12:06 -0500

    Update Redeem.lua

--- Redeem.lua
+++ Redeem.lua
@@ -1,7 +1,7 @@
 if (_G.Codes) then
-	local MainEvent = game.ReplicatedStorage.MainEvent
+	local MainEvent = game.ReplicatedStorage.MainEvent;
 	for i,Code in pairs(_G.Codes) do
-		MainEvent:FireServer('EnterPromoCode',Code)
-		task.wait(2)
-	end
-end
+		MainEvent:FireServer('EnterPromoCode',Code);
+		task.wait(2);
+	end;
+end;
