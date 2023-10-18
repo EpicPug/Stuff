244da2b801f7ba1e3ff0c6bfa3004213732d3092
EpicPug <112693990+EpicPug@users.noreply.github.com>
2023-10-17 22:49:16 -0500

    Update AntiAfk.lua

--- AntiAfk.lua
+++ AntiAfk.lua
@@ -4,7 +4,7 @@ local CurrentCamera = game:GetService('Workspace').CurrentCamera;
 local VirtualUser = game:GetService('VirtualUser');
 
 function Idled()
-	for i = 1,2 do
+	for i = 1,10 do
 		VirtualUser:Button2Down(Vector2.new(0,0), CurrentCamera.CFrame);
 		task.wait(1);
 		VirtualUser:Button2Up(Vector2.new(0,0), CurrentCamera.CFrame);
