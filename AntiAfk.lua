9f4162bd31dbd94d9d91ceaa63274bba39ce2386
EpicPug <112693990+EpicPug@users.noreply.github.com>
2024-07-27 18:27:51 -0700

    Update AntiAfk.lua

--- AntiAfk.lua
+++ AntiAfk.lua
@@ -1,7 +1,9 @@
-game.Players.LocalPlayer.Idled:Connect(function()
+local Player = game:GetService("Players").LocalPlayer;
+
+Player.Idled:Connect(function()
 	for i = 1, 15 do
 		game:GetService('VirtualUser'):Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
-		task.wait(0.25)
+		task.wait(0.5)
 		game:GetService('VirtualUser'):Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
 	end
-end)
+end);
