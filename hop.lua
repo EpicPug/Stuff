0efb5478ec5bcfa5b3f94b02696022e5ecbecfe7
EpicPug <112693990+EpicPug@users.noreply.github.com>
2024-08-17 15:51:42 -0700

    Update hop.lua

--- hop.lua
+++ hop.lua
@@ -1,23 +1,23 @@
-local Players = game:GetService('Players')
-local HttpService = game:GetService('HttpService')
+local Players = game:GetService("Players")
+local HttpService = game:GetService("HttpService")
 
 local TeleportService = game:GetService("TeleportService")
 
-local PlaceId = game.PlaceId
-local JobId = game.JobId
+local place_id = game.PlaceId
+local job_id = game.JobId
 
 local API = "https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=Desc&limit=100"
-
-local success, response
-local Table = {}
 
 return function()
 	local request = loadstring(game:HttpGet("https://raw.githubusercontent.com/EpicPug/Stuff/main/request.lua"))()
 	if request then
 		local data
-
+		
+		local success, response
+		local found_servers = {}
+		
 		success, response = pcall(function()
-			data = request({Url = API:format(PlaceId)})
+			data = request({Url = API:format(place_id)})
 		end)
 
 		if success and data and data.Body then
@@ -28,8 +28,8 @@ return function()
 
 			if success and decode and decode.data then
 				for _, found in pairs(decode.data) do
-					if type(found) == 'table' and found['id'] ~= JobId then
-						table.insert(Table, {
+					if type(found) == "table" and found["id"] ~= job_id then
+						table.insert(found_servers, {
 							["playing"] = found.playing,
 							["maxPlayers"] = found.maxPlayers,
 							["ping"] = found.ping,
@@ -38,11 +38,11 @@ return function()
 					end
 				end
 
-				if #Table > 0 then
+				if #found_servers > 0 then
 					local id
 					local lowest_ping = 100
 
-					for i, v in ipairs(Table) do
+					for i, v in ipairs(found_servers) do
 						if v["playing"] < v["maxPlayers"] then
 							if v["ping"] < lowest_ping then
 								id = v["id"]
@@ -53,11 +53,11 @@ return function()
 
 					if id then
 						TeleportService.TeleportInitFailed:Connect(function()
-							TeleportService:TeleportToPlaceInstance(PlaceId, id, Players.LocalPlayer)
+							TeleportService:TeleportToPlaceInstance(place_id, id, Players.LocalPlayer)
 						end)
 
 						repeat
-							TeleportService:TeleportToPlaceInstance(PlaceId, id, Players.LocalPlayer)
+							TeleportService:TeleportToPlaceInstance(place_id, id, Players.LocalPlayer)
 							task.wait(2)
 						until not game
 					end
