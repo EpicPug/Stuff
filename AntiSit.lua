a4cfb56d8fa449df4bcb864cae380ba29b148d28
EpicPug <112693990+EpicPug@users.noreply.github.com>
2025-02-14 03:32:54 -0600

    Update AntiSit.lua

--- AntiSit.lua
+++ AntiSit.lua
@@ -1,25 +1,21 @@
--- works i guess
-
-local Player = game:GetService("Players").LocalPlayer
+local Player = game:GetService('Players').LocalPlayer
 local Connection = nil
 
-pcall(function()
-	for i,v in pairs(game.Workspace:GetDescendants()) do
-		if v:IsA("Seat") or v:IsA("VehicleSeat") then
-			v:Destroy()
-		end
+for _, seat in pairs(workspace:GetDescendants()) do
+	if seat:IsA('Seat') or seat:IsA('VehicleSeat') then
+		seat:Destroy()
 	end
-end)
+end
 
-function idk()
-	if Connection ~= nil then
+local function Setup()
+	if Connection then
 		Connection:Disconnect()
 		Connection = nil
 	end
-	
-	local Char = Player.Character or Player.CharacterAdded:Wait()
-	local Humanoid = Char:WaitForChild('Humanoid')
-	
+
+	local Character = Player.Character or Player.CharacterAdded:Wait()
+	local Humanoid = Character:WaitForChild('Humanoid')
+
 	Connection = Humanoid.Seated:Connect(function()
 		if Humanoid.Sit then
 			Humanoid.Sit = false
@@ -28,6 +24,6 @@ function idk()
 	end)
 end
 
-task.spawn(idk)
+Setup()
 
-Player.CharacterAdded:Connect(idk)
+Player.CharacterAdded:Connect(Setup)
