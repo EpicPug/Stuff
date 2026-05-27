858682e21a270931e0f5971540c9ecf775a6dfcc
EpicPug <112693990+EpicPug@users.noreply.github.com>
2026-05-26 19:25:45 -0500

    Update and rename Redeem.lua to RedeemCodes.luau

--- Redeem.lua
+++
@@ -1,7 +0,0 @@
-local ReplicatedStorage = game:GetService('ReplicatedStorage')
-local MainEvent = ReplicatedStorage:WaitForChild('MainEvent')
-
-for _, str in ipairs(_G.Codes) do
-	MainEvent:FireServer('EnterPromoCode', str)
-	task.wait(3)
-end
